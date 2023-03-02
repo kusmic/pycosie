8a4fd493a72a19a59735643faf05b114ce8f4799
kusmic <samirkusmic@gmail.com>
2023-03-02 12:31:47 -0700

    "Fixed skid"

--- pycosie/cluster/skid.py
+++ pycosie/cluster/skid.py
@@ -11,7 +11,7 @@ class SkidCatalog():
         Object to hold the mass and position of galaxy groups found in SKID
         """
         
-        def __init__(self, statname, snapname=None, unit_base=None, bounding_box=None, cosmo=None, L=None):
+        def __init__(self, statname, snapname=None, unit_base=None, bounding_box=None):
                 """
                 For this catalog defaults, masses are in Msun and position is ckpc/h.
                 
@@ -34,36 +34,20 @@ class SkidCatalog():
                 # Check that only using snapname OR cosmo and L EXCLUSIVELY
                 # Then creating units for each case
                 if snapname==None:
-                        if cosmo==None or L==None:
-                                raise ValueError("Undefined parameters. Please either provide snapname (path to snapshot or yt Dataset) OR cosmo and L")
-                        else:
-                                H0 = cosmo.hubble_constant * 100. * u.km / u.Mpc / u.s
-                                rho_c = 3 * H0**2 / (8 * np.pi * c.G)
-                                rho_c = rho_c.to(u.g/u.cm**3)
-                                L = cosmo.arr(L, "kpccm/h")
-                                volSim = L.to("cmcm")**3
-                                massUnit = cosmo.arr(rho_c.value * volSim.value, "g").to("Msun")
+                        raise ValueError("Please provide the snapshot filename and path.")
                                 
                 if snapname!=None:
-                        if cosmo!=None or L!=None:
-                                raise RuntimeError("Ambiguous definition. Please define snapname OR cosmo and L, but not all three.")
+                        if type(snapname)==str:
+                                ds = yt.load(snapname, unit_base=unit_base, bounding_box=bounding_box)
                         else:
-                                if type(snapname)==str:
-                                        ds = yt.load(snapname, unit_base=unit_base, bounding_box=bounding_box)
-                                else:
-                                        ds = snapname
-                                H0 = ds.hubble_constant * 100. * u.km / u.Mpc / u.s
-                                co = Cosmology(omega_lambda=ds.omega_lambda,
-                                               omega_matter=ds.omega_matter,
-	                                       omega_radiation=0,
-	                                       omega_curvature=0,
-	                                       hubble_constant=ds.hubble_constant)
-                
-                                rho_c = 3* H0**2 / (8 * np.pi * c.G)
-                                rho_c = rho_c.to(u.g / u.cm**3)
-                                L = ds.domain_width.to("kpccm/h")[0]
-                                volSim = L.to("cmcm")**3
-                                massUnit = co.arr(rho_c.value * volSim.value, "g").to("Msun")
+                                ds = snapname
+                        H0 = ds.hubble_constant * 100. * u.km / u.Mpc / u.s
+
+                        rho_c = 3* H0**2 / (8 * np.pi * c.G)
+                        rho_c = rho_c.to(u.g / u.cm**3)
+                        L = ds.domain_width.to("kpccm/h")[0]
+                        volSim = L.to("cmcm")**3
+                        massUnit = ds.arr(rho_c.value * volSim.value, "g").to("Msun")
                 
                 ID, npart, totMass, gasMass, stlMass, vcmax, hvc, ovc, rmvc, rhm, outr, dv, x, y, z, vx, vy, vz, xb, yb, zb = np.loadtxt(statname, unpack=True)
                 pos = np.asarray([x, y, z]).T + 0.5
