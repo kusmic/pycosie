4dae995d70ad8b7e3eb33d360a1365413a7827ab
kusmic <samirkusmic@gmail.com>
2023-07-19 02:28:33 -0600

    Fixed mass fraction error initializing

--- pycosie/utils/galgrid.py
+++ pycosie/utils/galgrid.py
@@ -29,7 +29,7 @@ class GalaxyGrid():
                 star_SL_func = interp1d([91450.403, 188759.328], [0.100, 0.300], kind="linear", fill_value="extrapolate") # stellar mass in Msun to smoothing length in ckpc/h
 
             self.id = id
-            self.gasMetalDensityGrids = dict()
+            self.gasMetalMassFractionGrids = dict()
             # TD metallicities enumerated "Metallicity_0X"
             # 0:C, 1:O, 2:Si, 3:Fe, 4:N, 5:Ne, 6:Mg, 7:S, 8:Ca, 9:Ti, in that order
             if metals == None:
@@ -38,7 +38,7 @@ class GalaxyGrid():
                 __metalArr = metals
 
             for mi in range(len(__metalArr)):
-                self.gasMetalDensityGrids[__metalArr[mi]] = np.zeros((gridLength, gridLength, gridLength))
+                self.gasMetalMassFractionGrids[__metalArr[mi]] = np.zeros((gridLength, gridLength, gridLength))
 
             __gPartCoord = sp["PartType0","Coordinates"].to("kpccm/h").value # ckpc/h
             __sPartCoord = sp["PartType4","Coordinates"].to("kpccm/h").value
