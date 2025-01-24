9f8c4e9b193c9d37dc74b26e2df0fbc26c7b62e6
kusmic <samirkusmic@gmail.com>
2025-01-24 14:05:00 -0700

    Initiated RadialBinner.jl

---
+++ pycosie/utils/RadialBinner.jl
@@ -0,0 +1,8 @@
+#= Radial binner to bin gas information in each galaxy/halo radially.
+
+This needs to do maybe 3 functionalities
+1. Get isotropically averaged radial bins
+2. Get variance in each angle assuming a fixed axis
+    Likely assuming off x-axis (1st index)
+3(maybe). Get spherical bins in all 3 dimensions: radius and
+two angles.
