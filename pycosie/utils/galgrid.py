31fccb3c723b8f4266df693d6218407901655300
kusmic <samirkusmic@gmail.com>
2025-05-22 16:29:37 -0600

    Added tracking gas particle IDs in TestGalaxyGrid

--- pycosie/utils/galgrid.py
+++ pycosie/utils/galgrid.py
@@ -368,6 +368,7 @@ class TestGalaxyGrid():
                 __gPartZarr.append( sp["PartType0", f"Metallicity_{i:02}"].value ) # unitless
             self.gasParticleMetallicites = __gPartZarr # saving outputs to test/debug
             self.gasParticleDensity = sp["PartType0","density"].to("g/cm3").value
+            self.gasParticleIDs = sp["PartType0", "ParticleIDs"].value.astype(int)
 
             # So indexing of __gPartZarr is (index of species in metal_arr, index of gas particle)
 
